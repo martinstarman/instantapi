3ab04f0f922796affcaeb0b8621ff88f0465e663
ms <martin.starman@paninarodigital.com>
2017-04-07 17:12:53 +0200

    Base array support

--- test/response.js
+++ test/response.js
@@ -3,45 +3,63 @@ const response = require("../lib/response")
 
 describe("response", () => {
 
-  it("json", () => {
-    let json = {
-      num: 1,
-      str: "a",
-      str2: "1a"
-    }
-    let res = response(json)
-    assert.deepEqual(res, json)
+  it("simple object", () => {
+    let json = {a: 1, b: "a", c: "1"}
+    assert.deepEqual(response(json), json)
   })
 
-  it("json with object", () => {
-    let json = {
-      num: 1,
-      str: "a",
-      obj: {
-        num: 2,
-        str: "b"
-      }
-    }
-    let res = response(json)
-    assert.deepEqual(res, json)
+  it("simple array", () => {
+    let arr = [1, "a", "1"]
+    assert.deepStrictEqual(response(arr), arr)
   })
 
-  it("json with @num()", () => {
-    let res = response({
-      num: "@num(0, 5)"
-    })
-    assert.ok(res.num >= 0 && res.num < 5)
+  it("object with deeper object", () => {
+    let json = {a: 1, b: {c: "a", d: 1}}
+    assert.deepEqual(response(json), json)
   })
 
-  it("json with deeper @num()", () => {
-    let res = response({
-      num: "@num(0, 5)",
-      obj: {
-        num: "@num(10, 20)"
-      }
-    })
-    assert.ok(res.num >= 0 && res.num < 5)
-    assert.ok(res.obj.num >= 10 && res.obj.num < 20)
+  it("object with deeper array", () => {
+    let json = {a: 1, b: ["a", 1]}
+    assert.deepEqual(response(json), json)
+  })
+
+  it("array with deeper object", () => {
+    let json = [1, "a", {a: 1, b: "a"}]
+    assert.deepEqual(response(json), json)
+  })
+
+  it("array with deeper array", () => {
+    let json = [1, "a", [1, "a"]]
+    assert.deepEqual(response(json), json)
+  })
+
+  it("simple object with @num()", () => {
+    let json = {a: "@num(0,5)"}
+    let resp = response(json)
+    assert.ok(resp.a >= 0 && resp.a < 5)
+  })
+
+  it("simple array with @num()", () => {
+    let json = ["@num(0,5)"]
+    let resp = response(json)
+    assert.ok(resp.length === 1)
+    assert.ok(resp[0] >= 0 && resp[0] < 5)
+  })
+
+  it("@num() in deeper object", () => {
+    let json = {a: 1, b: {c: "@num(0,5)"}}
+    let resp = response(json)
+    assert.ok(resp.a === 1)
+    assert.ok(typeof resp.b === "object")
+    assert.ok(resp.b.c >= 0 && resp.b.c < 5)
+  })
+
+  it("@num() in deeper array", () => {
+    let json = {a: 1, b: ["@num(0,5)"]}
+    let resp = response(json)
+    assert.ok(resp.a === 1)
+    assert.ok(typeof resp.b === "object")
+    assert.ok(resp.b[0] >= 0 && resp.b[0] < 5)
   })
 
 })
